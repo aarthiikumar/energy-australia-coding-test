550414c555a609dcb7325e7e7dc95c61c66f16eb
aarthii.kumar <aarthi.kumar@crystaldelta.com>
2024-04-02 13:14:02 +0530

    added loggers refactored and added UT

--- src/main/java/com/energyaustralia/codingtest/service/FestivalService.java
+++ src/main/java/com/energyaustralia/codingtest/service/FestivalService.java
@@ -2,6 +2,9 @@ package com.energyaustralia.codingtest.service;
 
 import com.energyaustralia.codingtest.exception.CustomException;
 import com.energyaustralia.codingtest.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.http.HttpMethod;
@@ -10,31 +13,46 @@ import org.springframework.web.client.RestTemplate;
 import java.util.*;
 import org.springframework.core.ParameterizedTypeReference;
 
+
 @Service
 public class FestivalService {
+    private static final Logger logger = LoggerFactory.getLogger(FestivalService.class);
+
     @Value("${api.url}")
     private String apiUrl;
 
-    // Fetch list of festivals from api
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public FestivalService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public List<Festival> fetchFestivals() {
         try {
-            RestTemplate restTemplate = new RestTemplate();
-            return restTemplate.exchange(
+            logger.info("Fetching Festivals from API endpoint: {}", apiUrl);
+            List<Festival> festivals = restTemplate.exchange(
                     apiUrl,
                     HttpMethod.GET,
                     null,
-                    new ParameterizedTypeReference<List<Festival>>() {
-                    }
+                    new ParameterizedTypeReference<List<Festival>>() {}
             ).getBody();
+
+            logger.info("Fetched {} festivals", (festivals != null ? festivals.size() : 0));
+            return festivals;
         } catch (RestClientException e) {
+            logger.error("RestClientException occurred while fetching festivals: {}", e.getMessage());
             throw new CustomException("Error: Unable to retrieve data from server. " + e.getMessage());
         } catch (Exception e) {
+            logger.error("Unexpected error occurred while fetching festivals: {}", e.getMessage());
             throw new CustomException("An unexpected error occurred: " + e.getMessage());
         }
     }
 
     // Prepare the final list of record labels
     public List<RecordLabel> mapFestivalsToRecordLabels(List<Festival> festivals) {
+        logger.info("Mapping {} festivals to record labels", festivals.size());
+
         Map<String, Map<String, List<String>>> map = new HashMap<>();
 
         for(Festival festival : festivals){
@@ -44,6 +62,7 @@ public class FestivalService {
                         .add(festival.getName());
             }
         }
+        logger.debug("Map constructed: {}", map);
 
         List<RecordLabel> recordLabels = new ArrayList<>();
         for(Map.Entry<String, Map<String, List<String>>> mapEntry : map.entrySet()){
@@ -67,7 +86,7 @@ public class FestivalService {
             recordLabel.setBands(bandFestivals);
             recordLabels.add(recordLabel);
         }
-
+        logger.info("Mapped festivals to {} record labels", recordLabels.size());
         return recordLabels;
     }
 }
--- src/test/java/com/energyaustralia/codingtest/service/FestivalServiceTest.java
+++ src/test/java/com/energyaustralia/codingtest/service/FestivalServiceTest.java
@@ -1,21 +1,53 @@
 package com.energyaustralia.codingtest.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
 import java.util.List;
 import com.energyaustralia.codingtest.model.Band;
 import com.energyaustralia.codingtest.model.Festival;
 import com.energyaustralia.codingtest.model.RecordLabel;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import org.mockito.ArgumentMatchers;
+;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import org.junit.runner.RunWith;
+
+
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(properties = {"api.url=https://mock/festivals"})
 public class FestivalServiceTest {
+    private static final Logger logger = LoggerFactory.getLogger(FestivalServiceTest.class);
 
-    @InjectMocks
-    private FestivalService festivalService;
+    @MockBean
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private FestivalService service;
 
     @Test
     void testMapFestivalsToRecordLabels() {
-        festivalService = new FestivalService();
         Festival festival = new Festival();
         festival.setName("Festival1");
         Band band = new Band();
@@ -23,7 +55,7 @@ public class FestivalServiceTest {
         band.setRecordLabel("Record1");
         festival.setBands(List.of(band));
 
-        List<RecordLabel> result = festivalService.mapFestivalsToRecordLabels(List.of(festival));
+        List<RecordLabel> result = service.mapFestivalsToRecordLabels(List.of(festival));
         // Check that the result is not null
         assertNotNull(result);
 
@@ -32,5 +64,32 @@ public class FestivalServiceTest {
 
         // Check that the result contains the correct records
         assertEquals(result.get(0).getLabel(), band.getRecordLabel());
+    }
+
+    @Test
+    public void fetchFestivals() {
+
+        //given
+        Festival festival = new Festival();
+        festival.setName("Festival1");
+        Band band = new Band();
+        band.setName("Band1");
+        band.setRecordLabel("Record1");
+        festival.setBands(List.of(band));
+        List<Festival> expectedFestivals = new ArrayList<>();
+        expectedFestivals.add(festival);
+        ResponseEntity<List<Festival>> mockResponse = new ResponseEntity<>(expectedFestivals, HttpStatus.OK);
+        when(restTemplate.exchange(
+                eq("https://mock/festivals"),
+                eq(HttpMethod.GET),
+                eq(null),
+                        ArgumentMatchers.<ParameterizedTypeReference<List<Festival>>>any()))
+
+                .thenReturn(mockResponse);
+        //when
+        List<Festival> actualFestivals = service.fetchFestivals();
+
+        //then
+        assertEquals(expectedFestivals, actualFestivals);
     }
 }
